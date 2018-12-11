4dbc30e75c296c97b3f6b9c499056235e84989ba
Romian Tairovski <romian.tairovski@gmail.com>
2018-12-11 21:32:29 +0100

    add title

--- include/header.php
+++ include/header.php
@@ -8,6 +8,7 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="/templates/DynaHotel/include/style.css">
+		<title>DynaHotel - <?=str_replace("/", "", $_SERVER['REQUEST_URI'])?></title>
 	</head>
 	<body>
 	<div class="bigheader">
@@ -21,7 +22,7 @@
 	</div>
 	<div class="menuwrap">
 	<div class="container">
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light"> 
 			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 				<div class="navbar-nav">
 					<li>
