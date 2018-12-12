c8428f3f51547ea8a24484a5f5459f087bd46307
Romian Tairovski <romian.tairovski@gmail.com>
2018-12-12 22:17:15 +0100

    changed images to webp

--- include/header.php
+++ include/header.php
@@ -8,12 +8,13 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="/templates/DynaHotel/include/style.css">
-		<title>DynaHotel - <?=str_replace("/", "", $_SERVER['REQUEST_URI'])?></title>
+		<?php $remove = array("/", "home"); ?>
+		<title>DynaHotel - <?=str_replace($remove, "", $_SERVER['REQUEST_URI'])?></title>
 	</head>
 	<body>
 	<div class="bigheader">
 		<div class="logoholder">
-		<img class="logo" src="templates/DynaHotel/images/logo2.png">
+		<img class="logo" src="../templates/DynaHotel/images/logo2.webp">
 		</div>
 		<div class="headerbutton">
 			<button type="button" class="btn btn-success"><a class="link"href="../client">GA NAAR DYNA »</a></button>
@@ -26,21 +27,21 @@
 			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 				<div class="navbar-nav">
 					<li>
-						<a class="nav-link" href="../me">Home</a>
+						<a class="nav-link" href="../me">HOME</a>
 					</li>
 					<li>
-						<a class="nav-link" href="../home/{username}" >Je account</a>
+						<a class="nav-link" href="../home/{username}" >ACCOUNT</a>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link " href="../community" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Community</a>
+						<a class="nav-link " href="../community" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">COMMUNITY</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-							<a class="dropdown-item" href="../community">Community</a>
-							<a class="dropdown-item" href="../staff">Staff</a>
-							<a class="dropdown-item" href="../sollicitaties">Word staff</a>
+							<a class="dropdown-item" href="../community">COMMUNITY</a>
+							<a class="dropdown-item" href="../staff">STAFF</a>
+							<a class="dropdown-item" href="../sollicitaties">WORD STAFF</a>
 						</div>
 					</li>
 					<li>
-						<a class="nav-link" href="../buyvip">Koop VIP Poins</a>
+						<a class="nav-link" href="../buyvip">VIP</a>
 					</li>
 				</div>
 				<div class="navbar-nav ml-auto">
