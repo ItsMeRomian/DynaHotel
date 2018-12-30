78e7b1d5c7d4672dc6bc94943246b172d3778c7d
Romian Tairovski <romian.tairovski@gmail.com>
2018-12-30 14:12:41 +0100

    added static news to index.php
    might change later to use the database features of Brain

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<title>DynaHotel - Login</title>
 	</head>
 	<body>
+	<div class="container-fluid">
 		<div class="row wrap">
 			<div class="col-2"> </div>
 			<div class="col-4 text-center">
@@ -23,14 +24,23 @@
 					<input type="password" name="password" id="password" class="form-control" placeholder="Je wachtwoord" id="username" name="username">
 					<a href="register">Nog geen account?</a><br>
 					<button class="btn btn-lg btn-success btn-block" type="submit" name="login">LOGIN</button>
-					<p class="mt-5 mb-3 text-muted">&copy; DYNAHOTEL 2018, made with <3</p>
+					<p class="mt-5 mb-3 text-muted">&copy; DYNAHOTEL 2018, made with &hearts; </p>
 				</form>
 			</div>
 			<div class="col-1"> </div>
 			<div class="col-4 text-justify side pil">
-				<h2 class="pilhead">Welkom in Dyna!</h2>
+				<h2 class="pilhead"><img src="templates\DynaHotel\images\icons\star.gif"> Welkom in Dyna!</h2>
 				<img src="templates/DynaHotel/images/security/franksplaca.gif" style="float: right;">
 				<p>Here at Dyna Hotel, we are a small community hotel, just like Habbo but our coin currency is free! Along with friendly management that keep the hotel running smooth. Our bullying and advertising policy is kept at a strict level to prevent such occuring on the hotel. For more information on the rules, Click Here. We are here to give you the best ultimate experience for you and your friends. The community holds events and competitions to make it more of a fun place to hang out. </p>
+			</div>
+		</div>
+		<div class="row ">
+			<div class="col text-justify pil" style="margin-left: 40px; margin-right: 40px;">
+				<h2 class="pilhead"><img src="templates\DynaHotel\images\icons\mis.png"> Nieuws:</h2>
+				<img style="float: left;"src="templates\DynaHotel\images\staff\frank_04.gif">
+				<img style="float: right;"src="templates\DynaHotel\images\star.gif">
+				Here at Dyna Hotel, we are a small community hotel, just like Habbo but our coin currency is free! Along with friendly management that keep the hotel running smooth. Our bullying and advertising policy is kept at a strict level to prevent such occuring on the hotel. For more information on the rules, Click Here. We are here to give you the best ultimate experience for you and your friends. The community holds events and competitions to make it more of a fun place to hang out.
+				
 			</div>
 		</div>
 	</body>
