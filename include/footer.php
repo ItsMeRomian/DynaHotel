b6331865269ce925dec33a88469a33bc0b00d2e4
ItsMeRomian <romian.tairovski@gmail.com>
2019-06-18 12:06:21 +0200

    fixed blue link in footer

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-	<span style="float: left;">&copy; DYNAHOTEL 2014-2018<br><a href="#">TERMS</a> | <a href="#">PRIVACY</a></span>
+	<span style="float: left; color:white;">&copy; DYNAHOTEL 2014-2018<br><a style="color:white; text-decoration: underline;" href="#">TERMS</a> | <a style="color:white; text-decoration: underline;" href="#">PRIVACY</a></span>
 	<span style="float: right; text-align: right;">MADE WITH &hearts; BY ITSMEROMIAN AND KAZOO,<br>USING BRAIN 1.9.9 AND PLUS</span>
 </div>
 </body>
